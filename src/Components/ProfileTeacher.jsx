ef009556a3e3c84f84ef6ae680b6e34c19c6dd6c
Nico072006 <nicolasjime500@gmail.com>
2025-12-04 09:37:13 -0500

    Avances en Front

--- src/Components/ProfileTeacher.jsx
+++ src/Components/ProfileTeacher.jsx
@@ -3,6 +3,7 @@ import React from "react";
 
 const ProfileTeacher = () => {
   return <h1>Perfil del Profe</h1>;
+  <a href="">Start</a>
 };
 
 export default ProfileTeacher;  
--- src/Page/Index.jsx
+++ src/Page/Index.jsx
@@ -1,15 +1,45 @@
-import Robot from "../Components/Robot.jsx"
-import ImgRobot from "../Img/ImgRobot.jpg"
-import "../style/index.css"
+import Robot from "../Components/Robot.jsx";
+import ImgRobot from "../Img/ImgRobot.jpg";
+import { useState } from "react";
 
-function Start(){
-    return(
-        <div>
-            <h1>Welcome</h1>
-            <Robot image={ImgRobot} />
+import "../style/index.css";
+
+function Start() {
+
+    return (
+        <div className="inicioConta">
+
+            {/* Sidebar */}
+            <aside className="sidebar">
+                <h2 className="sidebarTitulo">Helps</h2>
+                <ul className="sidebarMenu">
+                    <li>Notification</li>
+                    <li>Private</li>
+                    <li>Topics</li>
+                </ul>
+
+                
+            </aside>
+
+            {/* Contenedor principal */}
+            <div className="content">
+
+                {/* Header */}
+                <header className="header">
+                    <div className="logo">BoardAI</div>
+                    <div className="PerfilIcon">👤</div>
+                </header>
+
+
+                {/* Robot Section */}
+                <section className="robotS">
+                    <Robot image={ImgRobot} />
+                </section>
+
+            </div>
+
         </div>
-        
-    )
+    );
 }
 
-export default Start
+export default Start;
